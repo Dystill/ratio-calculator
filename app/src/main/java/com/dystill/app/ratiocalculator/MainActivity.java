348aeb4ab307ec6313000e7f2aa350617431fbcb
James Robinson <jameslrob95@gmail.com>
2016-07-01 13:47:05 -0400

    Created a recyclerView using the previously created ratio card layout.

--- app/src/main/java/com/dystill/app/ratiocalculator/MainActivity.java
+++ app/src/main/java/com/dystill/app/ratiocalculator/MainActivity.java
@@ -2,12 +2,37 @@ package com.dystill.app.ratiocalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static List<double[]> RATIO_LIST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RATIO_LIST = new ArrayList<>();
+
+        // initialization for testing purposes
+        for(int i = 0; i < 4; i++) {
+
+            RATIO_LIST.add(new double[4]);
+
+            for (int j = 0; j < RATIO_LIST.get(0).length; j++) {
+                RATIO_LIST.get(i)[j] = 25 * j;
+            }
+        }
+
+        RecyclerView mainRatioList = (RecyclerView) findViewById(R.id.cardList);
+        RatioCardRecyclerAdapter mainRatioListAdapter = new RatioCardRecyclerAdapter(this, RATIO_LIST);
+
+        mainRatioList.setAdapter(mainRatioListAdapter);
+        mainRatioList.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }
---
+++ app/src/main/java/com/dystill/app/ratiocalculator/RatioCardRecyclerAdapter.java
@@ -0,0 +1,65 @@
+package com.dystill.app.ratiocalculator;
+
+import android.content.Context;
+import android.support.annotation.IntegerRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RatioCardRecyclerAdapter extends
+        RecyclerView.Adapter<RatioCardRecyclerAdapter.RatioCardHolder> {
+
+    private LayoutInflater inflater;
+    private List<double[]> ratioList;
+
+    public RatioCardRecyclerAdapter(Context context, List<double[]> rList) {
+        this.inflater = LayoutInflater.from(context);
+        ratioList = rList;
+    }
+
+    @Override
+    public RatioCardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.card_ratiotable, parent, false);
+        return new RatioCardHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RatioCardHolder holder, int position) {
+
+        // create string placeholders for each number
+        String text1 = "" + ratioList.get(position)[0];
+        String text2 = "" + ratioList.get(position)[1];
+        String text3 = "" + ratioList.get(position)[2];
+        String text4 = "" + ratioList.get(position)[3];
+
+        // set the text fo each text field
+        holder.topLeft.setText(text1);
+        holder.topRight.setText(text2);
+        holder.bottomLeft.setText(text3);
+        holder.bottomRight.setText(text4);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ratioList.size();
+    }
+
+    protected class RatioCardHolder extends RecyclerView.ViewHolder {
+
+        EditText topLeft, topRight, bottomLeft, bottomRight;
+
+        public RatioCardHolder(View itemView) {
+            super(itemView);
+            topLeft = (EditText) itemView.findViewById(R.id.numberTopLeft);
+            topRight = (EditText) itemView.findViewById(R.id.numberTopRight);
+            bottomLeft = (EditText) itemView.findViewById(R.id.numberBottomLeft);
+            bottomRight = (EditText) itemView.findViewById(R.id.numberBottomRight);
+        }
+    }
+}
