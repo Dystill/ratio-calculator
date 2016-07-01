dfac214831769a33e9386ffd2bf1b9aa92c13a97
James Robinson <jameslrob95@gmail.com>
2016-07-01 18:06:40 -0400

    Changed main activity to a basic layout with an inflater menu in the toolbar.

--- app/src/main/java/com/dystill/app/ratiocalculator/MainActivity.java
+++ app/src/main/java/com/dystill/app/ratiocalculator/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         RATIO_LIST = new ArrayList<>();
 
@@ -35,8 +41,33 @@ public class MainActivity extends AppCompatActivity {
         mainRatioList.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {                                                 ////// onCreateOptionsMenu() //////
+        getMenuInflater().inflate(R.menu.menu_main, menu);                                          // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {                                           ////// onOptionsItemSelected() //////
+        switch (item.getItemId()) {
+
+            case R.id.action_settings:                                                              // settings action button
+                return true;
+
+            case R.id.action_add:                                                                   // add folder action button
+                addRatio();
+                return true;
+
+            default:                                                                                // the user's action was not recognized.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     protected static void removeRatio(int folder) {
         RATIO_LIST.remove(folder);
     }
 
+    protected static void addRatio() {
+        RATIO_LIST.add(new double[4]);
+    }
 }
--- app/src/main/java/com/dystill/app/ratiocalculator/RatioCardRecyclerAdapter.java
+++ app/src/main/java/com/dystill/app/ratiocalculator/RatioCardRecyclerAdapter.java
@@ -1,14 +1,12 @@
 package com.dystill.app.ratiocalculator;
 
 import android.content.Context;
-import android.support.annotation.IntegerRes;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RatioCardRecyclerAdapter extends
